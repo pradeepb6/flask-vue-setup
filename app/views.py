56fcbc920638e619fa3cc482340efe53052e9e60
Pradeep Baradur <pradeepbaradur6@gmail.com>
2018-03-20 10:27:30 +0100

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, make_response
 
-from alkaweb import app
+from . import app
 
 
 @app.route('/')
@@ -8,7 +8,3 @@ def hello_world():
     resp = make_response(render_template('index.html'))
     return resp
 
-
-@app.route('/mail')
-def mail_send():
-    return 'Mail sent'
